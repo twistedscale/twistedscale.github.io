29c3dc46ad098736fb3158b955ea9e5395f24c88
twistedscale <info@toki1703.net>
2025-06-04 12:38:02 +0900

    Copyed from 8values-ja

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# twistedscale.github.io
-forked from 8values. A diagnostic that reveals your values and Twitter addiction level.
+# 8values-ja.github.io
+
+<a href="https://8values-ja.github.io/index.html" target="_blank">日本語版8values</a>は、8つの価値基準に基づき政治思想を診断するテスト<a href="https://8values.github.io/index.html" target="_blank">8values</a>を翻訳・ローカライズしたものです。現在、公式版も多言語対応を視野に入れて開発が続けられていますが、あまり進展が思わしくないのが現状です。公式版が日本語対応し次第、こちらのプロジェクトは凍結ないし削除する予定です。</p>
+<p>8values-ja(日本語版8values) is a translation/localisation of <a href="https://8values.github.io/index.html" target="_blank">8values</a>. We are planning to delete 8values-ja when (if?) the official localisation becomes available.
+  
+ # LICENSE
+<p>公式版8valueはMIT Licenseに基づいて公開されており、「無制限("without restriction")」に「ソフトウェアの複製を使用、複写、変更、結合、掲載、頒布、サブライセンス、および/または販売する ("use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software")」権利が認められています。日本語版8valuesも、同MIT License下にあります。</p>
+<p>8values is licensed under the MIT License, which permits "without restriction" the rights to "use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software". 8values-ja is also under the same license.</p>
---
+++ ideologies.js
@@ -0,0 +1,521 @@
+ideologies = [
+    {    
+	"name": "無政府共産主義",
+        "original": "Anarcho-Communism",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 90
+        }
+    },
+    {
+	"name": "リバタリアン共産主義",
+	"original": "Libertarian Communism",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {
+	"name": "トロツキー主義",
+        "original": "Trotskyism",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {
+	"name": "マルクス主義",
+        "original": "Marxism",
+        "stats": {
+            "econ": 100,
+            "dipl": 70,
+            "govt": 40,
+            "scty": 80
+        }
+    },
+    {
+	"name": "デ・レオン主義",
+        "original": "De Leonis",
+        "stats": {
+            "econ": 100,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 80
+        }
+    },
+    {
+	"name": "レーニン主義",
+        "original": "Leninism",
+        "stats": {
+            "econ": 100,
+            "dipl": 40,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+	"name": "スターリン主義／毛沢東主義",
+        "original": "Stalinism/Maoism",
+        "stats": {
+            "econ": 100,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 60
+        }
+    },
+    {
+	"name": "宗教的共産主義",
+	"original": "Religious Communism",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {
+	"name": "国家社会主義",
+        "original": "State Socialism",
+        "stats": {
+            "econ": 80,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+	"name": "神権主義的社会主義",
+        "original": "Theocratic Socialism",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {    
+	"name": "宗教的社会主義",
+        "original": "Religious Socialism",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 70,
+            "scty": 20
+        }
+    },
+    {    
+	"name": "民主社会主義者",
+        "original": "Democratic Socialism",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 80
+        }
+    },
+    {   
+	"name": "革命主義的社会主義",
+        "original": "Revolutionary Socialism",
+        "stats": {
+            "econ": 80,
+            "dipl": 20,
+            "govt": 50,
+            "scty": 70
+        }
+    },
+    {   
+	"name": "リバタリアン社会主義",
+        "original": "Libertarian Socialism",
+        "stats": {
+            "econ": 80,
+            "dipl": 80,
+            "govt": 80,
+            "scty": 80
+        }
+    },
+    {   
+	"name": "無政府組合主義",
+        "original": "Anarcho-Syndicalism",
+        "stats": {
+            "econ": 80,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 80
+        }
+    },
+    {   
+	"name": "左翼ポピュリズム",
+        "original": "Left-Wing Populism",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {   
+	"name": "神権的分産主義",
+        "original": "Theocratic Distributism",
+        "stats": {
+            "econ": 60,
+            "dipl": 40,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {   
+	"name": "分産主義",
+        "original": "Distributism",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 20
+        }
+    },
+    {   
+	"name": "社会自由主義",
+        "original": "Social Liberalism",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {   
+	"name": "キリスト教民主主義",
+        "original": "Christian Democracy",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {   
+	"name": "社会民主主義",
+        "original": "Social Democracy",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {   
+	"name": "進歩主義",
+        "original": "Progressivism",
+        "stats": {
+            "econ": 60,
+            "dipl": 80,
+            "govt": 60,
+            "scty": 100
+        }
+    },
+    {   
+	"name": "無政府相互主義",
+        "original": "Anarcho-Mutualism",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 70
+        }
+    },
+    {   
+	"name": "一国全体主義",
+        "original": "National Totalitarianism",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {   
+	"name": "超国家全体主義",
+        "original": "Global Totalitarianism",
+        "stats": {
+            "econ": 50,
+            "dipl": 80,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {   
+	"name": "技術官僚主義",
+        "original": "Technocracy",
+        "stats": {
+            "econ": 60,
+            "dipl": 60,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {   
+	"name": "中道主義",
+        "original": "Centrist",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {   
+	"name": "自由主義（リベラリズム）",
+        "original": "Liberalism",
+        "stats": {
+            "econ": 50,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 60
+        }
+    },
+    {   
+	"name": "宗教的アナキズム",
+        "original": "Religious Anarchism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 20
+        }
+    },
+    {   
+	"name": "右翼ポピュリズム",
+        "original": "Right-Wing Populism",
+        "stats": {
+            "econ": 40,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 30
+        }
+    },
+    {   
+	"name": "中道保守主義",
+        "original": "Moderate Conservatism",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 30
+        }
+    },
+    {   
+	"name": "復古主義",
+        "original": "Reactionary",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 10
+        }
+    },
+    {   
+	"name": "左派リバタリアニズム",
+        "original": "Social Libertarianism",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 80,
+            "scty": 70
+        }
+    },
+    {   
+	"name": "自由至上主義（リバタリアニズム）",
+        "original": "Libertarianism",
+        "stats": {
+            "econ": 40,
+            "dipl": 60,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {   
+	"name": "エゴイスト・アナキズム",
+        "original": "Anarcho-Egoism",
+        "stats": {
+            "econ": 40,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {   
+	"name": "ナチズム",
+        "original": "Nazism",
+        "stats": {
+            "econ": 40,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+    {   
+	"name": "専制主義",
+        "original": "Autocracy",
+        "stats": {
+            "econ": 50,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {   "name": "ファシズム",
+        "original": "Fascism",
+        "stats": {
+            "econ": 40,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+	"name": "資本主義的ファシズム",
+        "original": "Capitalist Fascism",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 20,
+            "scty": 20
+        }
+    },
+    {
+	"name": "保守主義",
+        "original": "Conservatism",
+        "stats": {
+            "econ": 30,
+            "dipl": 40,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {   
+	"name": "新自由主義",
+        "original": "Neo-Liberalism",
+        "stats": {
+            "econ": 30,
+            "dipl": 30,
+            "govt": 50,
+            "scty": 60
+        }
+    },
+    {   
+	"name": "古典的自由主義",
+        "original": "Classical Liberalism",
+        "stats": {
+            "econ": 30,
+            "dipl": 60,
+            "govt": 60,
+            "scty": 80
+        }
+    },
+    {   
+	"name": "権威主義的資本主義",
+        "original": "Authoritarian Capitalism",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 20,
+            "scty": 40
+        }
+    },
+    {   
+	"name": "国家資本主義",
+        "original": "State Capitalism",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 30,
+            "scty": 50
+        }
+    },
+    {   
+	"name": "新保守主義",
+        "original": "Neo-Conservatism",
+        "stats": {
+            "econ": 20,
+            "dipl": 20,
+            "govt": 40,
+            "scty": 20
+        }
+    },
+    {   
+	"name": "市場原理主義",
+        "original": "Fundamentalism",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 30,
+            "scty": 5
+        }
+    },
+    {
+	"name": "右派リバタリアニズム",
+        "original": "Libertarian Capitalism",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 80,
+            "scty": 60
+        }
+    },
+    {
+	"name": "市場無政府主義",
+        "original": "Market Anarchism",
+        "stats": {
+            "econ": 20,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {   
+	"name": "客観主義（オブジェクティビズム）",
+        "original": "Objectivism",
+        "stats": {
+            "econ": 10,
+            "dipl": 50,
+            "govt": 90,
+            "scty": 40
+        }
+    },
+    {
+	"name": "全体主義的資本主義",
+	"original": "Totalitarian Capitalism",
+        "stats": {
+            "econ": 0,
+            "dipl": 30,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+	"name": "ウルトラ資本主義",
+        "original": "Ultra-Capitalism",
+        "stats": {
+            "econ": 0,
+            "dipl": 40,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+	"name": "無政府資本主義",
+        "original": "Anarcho-Capitalism",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    }
+];
